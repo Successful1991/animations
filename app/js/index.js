ec53b9d9c575125f58b4bb961abbf09e7df54882
successful1991 <successful1991@gmail.com>
2018-04-09 15:14:34 +0300

    two commit

--- app/js/index.js
+++ app/js/index.js
@@ -76,40 +76,10 @@ VideoPlayer.prototype.init = function () {
     $ ('#animate4 .animate4__line2').text (
       self.data.animate4.line2
     );
-
-
-
-
-
-    // $ ('.js-recent_pmt_date .calendar__data .num').text (
-    //   self.data.recent_pmt_date.date
-    // );
-    // $ ('.js-recent_pmt_date .calendar__data .year').text (
-    //   self.data.recent_pmt_date.year
-    // );
-    // $ ('.js-due_date .calendar__month').text (self.data.due_date.month);
-    // $ ('.js-due_date .calendar__data .num').text (self.data.due_date.date);
-    // $ ('.js-due_date .calendar__data .year').text (self.data.due_date.year);
-    // self.divideWordIntoLetters (self.data.month);
     CHARLIE.setup (video);
     return;
   });
 
-  // $ ('.charlie').on (self.animationStart, function (el) {
-  //   if (this.id === 'amount') {
-  //     self.numberAnimation (self.data.total_charges, 0, 50, this);
-  //   } else if (this.id === 'textAnimate4amount') {
-  //     self.numberAnimation (self.data.recent_pmt_amt, 0, 50, this);
-  //   } else if (this.id === 'textAnimate5amount') {
-  //     self.numberAnimation (self.data.overall_balance, 0, 50, this);
-  //   } else if (this.id === 'textAnimate6amount') {
-  //     self.numberAnimation (self.data.min_payment, 0, 50, this);
-  //   } else if (this.id === 'textAnimate7__amount1') {
-  //     self.numberAnimation (self.data.points_earned_month, 0, 50, this);
-  //   } else if (this.id === 'textAnimate7__amount2') {
-  //     self.numberAnimation (self.data.total_points_earned, 0, 50, this);
-  //   }
-  // });
   videoPlayerWrapper.append (video);
   self.myPlayer = videojs ('js--video-player', {
     controls: true,
@@ -138,53 +108,6 @@ VideoPlayer.prototype.init = function () {
 var vPlayer = new VideoPlayer (),
   video = vPlayer.video,
   textAnimationBlock = document.getElementById ('textAnimationBlock');
-
-// VideoPlayer.prototype.numberAnimation = function (
-//   amount,
-//   delay,
-//   duration,
-//   parent
-// ) {
-//   var options = {
-//     amount: amount,
-//     delay: delay,
-//     duration: duration,
-//   };
-//   var amount = options.amount;
-//   var time = amount / options.duration;
-//   var number = 0;
-//   var fixed = 0;
-//   if (amount.toString ().split ('.')[1]) {
-//     fixed = amount.toString ().split ('.')[1].length;
-//   }
-//   requestAnimationFrame (function interval () {
-//     number += time;
-//     parent.querySelector ('.number').innerHTML =
-//       // Math.round(number * 100) / 100;
-//       number.toFixed (fixed);
-//     if (number >= amount) {
-//       document.querySelector ('.number').innerHTML = amount;
-//       cancelAnimationFrame (interval);
-//     } else {
-//       requestAnimationFrame (interval);
-//     }
-//   });
-// };
-
-// VideoPlayer.prototype.divideWordIntoLetters = function (month) {
-//   var word = month;
-//   var str = word.split ('');
-//   $.each (str, function (index) {
-//     // идем по массиву
-//     $ ('#textAnimate2').append (
-//       '<span class="charlie" data-animations="textAnimateLetter" data-times="2.' +
-//       index +
-//       '">' +
-//       (this == ' ' ? '&nbsp;' : this) +
-//       '</span>'
-//     );
-//   });
-// };
 
 $ (document).ready (function () {
   video.addEventListener ('loadedmetadata', function () {
