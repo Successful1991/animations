0639326ae84c8549f62d4fc17f4fc336100380e3
successful1991 <successful1991@gmail.com>
2018-04-22 15:45:45 +0300

    first commit

---
+++ app/js/index.js
@@ -0,0 +1,423 @@
+'use strict';
+window.HELP_IMPROVE_VIDEOJS = false;
+
+function VideoPlayer() {
+  var videoElem = document.createElement('VIDEO');
+  videoElem.setAttribute('src', './app/img/personalized2.mov');
+  videoElem.setAttribute('class', 'video-js vjs-fluid');
+  videoElem.setAttribute('webkit-playsinline', '');
+  videoElem.setAttribute('playsinline', '');
+  videoElem.setAttribute('id', 'js--video-player');
+  this.video = videoElem;
+  console.log(videoElem);
+}
+
+
+VideoPlayer.prototype.animationStart = (function(el) {
+  var animations = {
+    animation: 'animationstart',
+    OAnimation: 'oAnimationStart',
+    MozAnimation: 'mozAnimationStart',
+    WebkitAnimation: 'webkitAnimationStart',
+  };
+  for (var t in animations) {
+    if (el.style[t] !== undefined) {
+      return animations[t];
+    }
+  }
+})(document.createElement('div'));
+
+
+VideoPlayer.prototype.animationEnd = (function(el) {
+  var animations = {
+    animation: 'animationend',
+    OAnimation: 'oAnimationEnd',
+    MozAnimation: 'mozAnimationEnd',
+    WebkitAnimation: 'webkitAnimationEnd',
+  };
+  for (var t in animations) {
+    if (el.style[t] !== undefined) {
+      return animations[t];
+    }
+  }
+  animateFinish();
+})(document.createElement('div'));
+
+VideoPlayer.prototype.fetchData = function(uri, callback) {
+  var self = this;
+  fetch(uri)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      self.data = myJson;
+      callback();
+    });
+};
+
+// VideoPlayer.prototype.init = function() {
+//     var self = this;
+//     var video = self.video;
+//     let params = (new URL(document.location)).searchParams;
+//     let name = params.get("id");
+
+//     //this.fetchData ('data.json', function callback () {
+//     this.fetchData('http://35.200.136.182:8083/video/' + name, function callback() {
+//         console.log(self.data);
+//         $('.js-name').text(self.data.text1);
+//         $('.js-month').text(self.data.text2);
+//         $('#animate3 .animate3__line2').append(
+//             self.data.text3
+//         );
+//         CHARLIE.setup(video);
+//         return;
+//     });
+
+VideoPlayer.prototype.init = function() {
+  var self = this;
+  var video = self.video;
+  this.fetchData('data.json', function callback() {
+     $('#animate1-text1').text(self.data.text11);
+     $('#animate1-text2').text(self.data.text12);
+     $('#animate1-text3').text(self.data.text13);
+     $('#animate1-img1').css({"background": "url("+ self.data.img1 +") center no-repeat","background-size": "contain"});
+
+    $('#animate2-text').text(self.data.text21);
+
+    $('#animate2-line1-text1').text(self.data.text211);
+    $('#animate2-line1-amount1').text(self.data.amount211);
+    $('#animate2-line1-amount2').text(self.data.amount212);
+
+    $('#animate2-line2-text1').text(self.data.text221);
+    $('#animate2-line2-amount1').text(self.data.amount221);
+    $('#animate2-line2-amount2').text(self.data.amount222);
+
+    $('#animate2-line3-text1').text(self.data.text231);
+    $('#animate2-line3-amount1').text(self.data.amount231);
+    $('#animate2-line3-amount2').text(self.data.amount232);
+
+    $('#animate3-text').text(self.data.text3);
+
+    $('#animate4-text1').text(self.data.text41);
+    //$('#animate4-amount1').text(self.data.amount41);
+    $('#animate4-amount2').append(self.data.amount41);
+
+
+    $('#animate5').text(self.data.text5);
+    // self.divideWordIntoLetters (self.data.text1,"#textAnimate1",4.1);
+    // self.divideWordIntoLetters (self.data.text2,"#textAnimate2",4.3);
+
+
+
+
+
+
+    $ ('.charlie').on (self.animationStart, function (el) {
+      if (this.id === 'animate4-amount1') {
+        self.numberAnimation (self.data.amount41, 0, 50, this);
+      }else(console.log("не сработало"))
+    });
+
+
+
+
+
+
+    // retargeting video element
+    video = document.getElementsByClassName('vjs-tech')[0];
+    CHARLIE.setup(video);
+    return;
+  });
+
+
+  $('#videoPlayerWrapper').append(video);
+  self.myPlayer = videojs('js--video-player', {
+    controls: true,
+    autoplay: false,
+    preload: false,
+  });
+
+  self.myPlayer.el_.addEventListener(
+    'webkitfullscreenchange',
+    function() {
+      self.handleFullScreen.call(this, event)
+    }
+  );
+  var currentTime = 0;
+
+  //This example allows users to seek backwards but not forwards.
+  //To disable all seeking replace the if statements from the next
+  //two functions with myPlayer.currentTime(currentTime);
+
+  self.myPlayer.on('seeking', function(event) {
+    if (currentTime < self.myPlayer.currentTime()) {
+      self.myPlayer.currentTime(currentTime);
+    }
+  });
+
+  self.myPlayer.on('seeked', function(event) {
+    if (currentTime < self.myPlayer.currentTime()) {
+      self.myPlayer.currentTime(currentTime);
+    }
+  });
+  self.myPlayer.on('ended', function() {
+    self.myPlayer.posterImage.show();
+    $(this.posterImage.contentEl()).show();
+    $(this.bigPlayButton.contentEl()).show();
+    self.myPlayer.currentTime(0);
+    self.myPlayer.controlBar.hide();
+    self.myPlayer.bigPlayButton.show();
+    self.myPlayer.cancelFullScreen();
+  });
+  self.myPlayer.on('play', function() {
+    self.myPlayer.posterImage.hide();
+    self.myPlayer.controlBar.show();
+    self.myPlayer.bigPlayButton.hide();
+  });
+
+  $("#textAnimationBlock a").on('click',function () {
+    self.myPlayer.pause();
+  });
+
+
+
+};
+
+
+
+
+
+
+
+
+
+VideoPlayer.prototype.numberAnimation = function (
+  amount,
+  delay,
+  duration,
+  parent
+) {
+  var options = {
+    amount: amount,
+    delay: delay,
+    duration: duration,
+  };
+  var amount = options.amount;
+  var time = amount / options.duration;
+  var number = 0;
+  var fixed = 0;
+  if (amount.toString ().split ('.')[1]) {
+    fixed = amount.toString ().split ('.')[1].length;
+  }
+  requestAnimationFrame (function interval () {
+    number += time;
+    parent.querySelector ('.number').innerHTML =
+      // Math.round(number * 100) / 100;
+      number.toFixed (fixed);
+    if (number >= amount) {
+      document.querySelector ('.number').innerHTML = amount;
+      cancelAnimationFrame (interval);
+    } else {
+      requestAnimationFrame (interval);
+    }
+  });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var vPlayer = new VideoPlayer(),
+  video = vPlayer.video,
+  textAnimationBlock = document.getElementById('textAnimationBlock');
+
+
+VideoPlayer.prototype.handleFullScreen = function(event) {
+  var self = this;
+  console.log('handleFullScreen', event);
+  /* Fullscreen */
+  lockScreenInLandscape();
+
+  function requestFullscreenVideo() {
+    if (videoPlayerWrapper.requestFullscreen) {
+      videoPlayerWrapper.requestFullscreen();
+    } else {
+      video.webkitEnterFullscreen();
+    }
+  }
+
+  if ('orientation' in screen) {
+    screen.orientation.addEventListener('change', function() {
+      // Let's automatically request fullscreen if user switches device in landscape mode.
+      if (screen.orientation.type.startsWith('landscape')) {
+        // Note: It may silently fail in browsers that don't allow requesting
+        // fullscreen from the orientation change event.
+        // https://github.com/whatwg/fullscreen/commit/e5e96a9da944babf0e246980559cd80a46a300ca
+        requestFullscreenVideo();
+      } else if (document.fullscreenElement) {
+        document.exitFullscreen();
+      }
+    });
+  }
+
+  function lockScreenInLandscape() {
+    if (!('orientation' in screen)) {
+      return;
+    }
+
+    // Let's force landscape mode only if device is in portrait mode and can be held in one hand.
+    if (
+      matchMedia('(orientation: portrait) and (max-device-width: 768px)')
+        .matches
+    ) {
+      screen.orientation.lock('landscape').then(function() {
+        // When screen is locked in landscape while user holds device in
+        // portrait, let's use the Device Orientation API to unlock screen only
+        // when it is appropriate to create a perfect and seamless experience.
+        listenToDeviceOrientationChanges();
+      });
+    }
+  }
+
+  function listenToDeviceOrientationChanges() {
+    if (!('DeviceOrientationEvent' in window)) {
+      return;
+    }
+
+    var previousDeviceOrientation, currentDeviceOrientation;
+    window.addEventListener(
+      'deviceorientation',
+      function onDeviceOrientationChange(event) {
+        // event.beta represents a front to back motion of the device and
+        // event.gamma a left to right motion.
+        if (Math.abs(event.gamma) > 10 || Math.abs(event.beta) < 10) {
+          previousDeviceOrientation = currentDeviceOrientation;
+          currentDeviceOrientation = 'landscape';
+          return;
+        }
+        if (Math.abs(event.gamma) < 10 || Math.abs(event.beta) > 10) {
+          previousDeviceOrientation = currentDeviceOrientation;
+          // When device is rotated back to portrait, let's unlock screen orientation.
+          if (previousDeviceOrientation == 'landscape') {
+            screen.orientation.unlock();
+            window.removeEventListener(
+              'deviceorientation',
+              onDeviceOrientationChange
+            );
+          }
+        }
+      }
+    );
+  }
+};
+
+
+
+// };
+// var vPlayer = new VideoPlayer (),
+//   video = vPlayer.video,
+//   textAnimationBlock = document.getElementById ('textAnimationBlock');
+
+//проходим по массиву разбиваем данные по буквам в отдельный span
+// VideoPlayer.prototype.divideWordIntoLetters = function (month) {
+//   var word = month;
+//   var str = word.split ('');
+//   $.each (str, function (index) {
+//     // идем по массиву
+//     $ ('#textAnimate3_2').append (
+//       '<span class="charlie" data-animations="textAnimateLetter" data-times="18.' +
+//       index +
+//       '">' +
+//       (this == ' ' ? '&nbsp;' : this) +
+//       '</span>'
+//     );
+//   });
+// };
+VideoPlayer.prototype.divideWordIntoLetters = function (month,id,time) {
+  var word = month;
+  var str = word.split ('');
+  var i = 0;
+  $.each (str, function (index) {
+    // идем по массиву
+    if(i*2 >= 10){
+      var x =("" + time).split(".")[1];
+      if(+x === 9){
+        time += 0.1;
+        i=0;
+        time = +time.toFixed(2);
+      }else{
+        time += 0.1;
+        i=0;
+        time = +time.toFixed(2);
+      }
+    };
+
+    $(id).append (
+      '<span class="charlie" data-animations="textAnimateLetter" data-times=" '+ time +
+      (i*2) +
+      '">' +
+      (this == ' ' ? '&nbsp;' : this) +
+      '</span>'
+    );
+    i++;
+  });
+};
+
+
+$(document).ready(function() {
+
+  vPlayer.init();
+  $('.vjs-fluid').append(textAnimationBlock);
+  textAnimationBlock.classList.add('is-ready');
+
+
+  // detect iOS full screen
+  var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+
+  if(iOS) {
+    $('.vjs-fullscreen-control').hide();
+  }
+
+  var ua = navigator.userAgent.toLowerCase();
+  var isAndroid = ua.indexOf("android") > -1;
+  if (isAndroid) {
+    $('.vjs-fullscreen-control').hide();
+  }
+
+  // iOS special treatment
+  var vidEl = document.getElementsByClassName('vjs-tech')[0];
+  vidEl.addEventListener('pause', function() {
+
+    if(iOS) {
+      $('.charlie').each(function(){
+        if ($(this).hasClass('animated')) {
+          $(this).css('-webkit-transform', $(this).css('-webkit-transform'))
+        }
+      })
+    }
+  })
+
+  //controlbar at bottom
+  function controlbarAtBottom() {
+    var height = $('.vjs-control-bar').height();
+    $('.vjs-control-bar').css('bottom', '-' + height + 'px');
+  }
+  controlbarAtBottom();
+  window.addEventListener('resize', controlbarAtBottom);
+  window.addEventListener('orientationchange', controlbarAtBottom);
+});
